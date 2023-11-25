a64eed9ef1f99112dc08eb4d86813e69188fc6e2
Aishiuu <151710092+Aishiuu@users.noreply.github.com>
2023-11-25 10:45:13 +0530

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/home")
-def home():
-    return{"success":True,"message":"Hello mcc!"}
-
-@app.get("/about")
-def about():
-    return {"Name":"Aish","Location":"Mumbai"}
+a
